64e2bc523a103ba9d65deb3001c47c9f4fc43832
Cayde Xu <cayde.xu@oocl.com>
2019-08-19 12:42:59 +0800

    Four Of a Kind compare Straight and Full House test and its impletments

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -133,4 +133,13 @@ public class PokerTest {
 
     }
 
+    @Test
+    public void should_return_2_when_given_1_3H3D3S5C3D_and_2_3H4H5H6H7H() {
+
+
+        ArrayList<PokerUtil> oneList = PokerUtil.generateCard("3H3D3S5C3D");
+        ArrayList<PokerUtil> twoList = PokerUtil.generateCard("3H4H5H6H7H");
+        assertEquals("2", Poker.winnerJudge(oneList, twoList));
+
+    }
 }
